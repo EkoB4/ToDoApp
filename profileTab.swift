0278c2af54de8d2e41c2b4aa1823bda53f2fa198
Ekin Bacik <ekinbacik@gmail.com>
2021-11-27 13:54:44 +0300

    new code added

--- profileTab.swift
+++ profileTab.swift
@@ -10,43 +10,22 @@ struct profileTab : View {
     var body: some View{
         HStack{
             GeometryReader{geo in
-                Circle()
+                /*Circle()
                     .frame(width:geo.size.width*3, height: geo.size.height*2)
-                    .foregroundColor(.red)
+                    .foregroundColor(.red)*/
+                Rectangle()
+                    .frame(width: geo.size.width*1, height: geo.size.height*0.4)
+                    .ignoresSafeArea()
+                    .foregroundColor(.purple)
                 imageView()
-                    .frame(width : geo.size.width*1,height:  geo.size.height*0.4)
+                    .frame(width : geo.size.width*1,height:  geo.size.height*0.7)
                 Text("Ekin Can Bacik")
                     .font(.title)
                     .bold()
-                    .frame(width:geo.size.width*1, height: geo.size.height*1)
+                    .frame(width:geo.size.width*1, height: geo.size.height*1.1)
                 Spacer()
-                Text("Github")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .bold()
-                    .frame(width: geo.size.width*0.4, height: geo.size.height*1.2)
-                    .background(RoundedRectangle(cornerRadius: 20)
-                                    .foregroundColor(.black)
-                                    .frame(width:geo.size.width*0.3,height: geo.size.height*0.1))
-                
-                Text("Instagram")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .bold()
-                    .frame(width: geo.size.width*1.5, height: geo.size.height*1.2)
-                    .background(RoundedRectangle(cornerRadius: 20)
-                                    .fill(LinearGradient(gradient: Gradient(colors: [.orange,Color("rectanglesColor")]), startPoint: .topTrailing, endPoint: .bottomTrailing))
-                                    .frame(width:geo.size.width*0.4,height: geo.size.height*0.1))
-                Text("Instagram")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .bold()
-                    .frame(width: geo.size.width*0.5, height: geo.size.height*1.7)
-                    .background(RoundedRectangle(cornerRadius: 20)
-                                    .fill(LinearGradient(gradient: Gradient(colors: [.orange,Color("rectanglesColor")]), startPoint: .topTrailing, endPoint: .bottomTrailing))
-                                    .frame(width:geo.size.width*0.4,height: geo.size.height*0.1))
-                    Spacer()
-            }
+               
+             }
         }
         }
 }
@@ -61,33 +40,33 @@ struct imageView : View {
         Image("test")
             .frame(width: 200, height: 200)
             .clipShape(Circle())
-            .overlay(Circle().stroke(Color("overlayColor") , lineWidth: 4))
+            .overlay(Circle().stroke(.white,lineWidth: 4))
     }
 }
 
 struct gitIcon : View {
     var body: some View{
+        VStack{
         GeometryReader{pos in
-            Image("git")
+            Rectangle()
+                .frame(width:pos.size.width*0.8, height:pos.size.height*0.2)
+                .foregroundColor(.clear)
+                .background(LinearGradient(gradient: Gradient(colors: [.white,.black]), startPoint: .topLeading, endPoint: .bottomTrailing)).cornerRadius(20)
+                Text("Github")
+                .bold()
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width:pos.size.width*0.3, height: pos.size.height*0.1)
+            Text("EkoB4")
+                .bold()
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width:pos.size.width*0.2, height: pos.size.height*0.3)
+           /* Image("git")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 100, height: 200)
-            Text("Github")
-                .font(.title)
-                .bold()
-                .frame(width: pos.size.width*0.8, height: pos.size.height*0.2)
-                Spacer()
-            Text("https://github.com/EkoB4")
-                .bold()
-                .foregroundColor(.white)
-                .background(RoundedRectangle(cornerRadius: 20)
-                                .foregroundColor(.black)
-                                .frame(width: pos.size.width*0.55, height: pos.size.height/30))
-                .frame(width: pos.size.width*1.1, height: pos.size.height*0.34)
-            /*Text("EkoB4")
-                    .font(.subheadline)
-                    .bold()
-                    .frame(width: pos.size.width*0.7, height: pos.size.height*0.4)*/
+                .frame(width: pos.size.width*0.4, height: pos.size.height*0.1)*/
+          }
         }
     }
 }
